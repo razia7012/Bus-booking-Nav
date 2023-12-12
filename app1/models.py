f97a00ea64e02c9946cebc331e741fbbaf9818bf
naveenapadmakumar <naveena@codesvera.com>
2023-12-12 19:34:13 +0530

    updated models and created bus list template

--- app1/models.py
+++ app1/models.py
@@ -4,14 +4,7 @@ from django.db import models
 
 from django.contrib.auth.models import User
 
-class Bus(models.Model):
-    bus_number = models.CharField(max_length=20)
-    capacity = models.PositiveIntegerField()
-    type_choices = [('AC', 'AC'), ('Non-AC', 'Non-AC')]
-    bus_type = models.CharField(max_length=10, choices=type_choices)
-
-    def __str__(self):
-        return self.bus_number
+from django.db import models
 
 class Route(models.Model):
     origin = models.CharField(max_length=50)
@@ -21,6 +14,27 @@ class Route(models.Model):
 
     def __str__(self):
         return f"{self.origin} to {self.destination}"
+
+
+class Bus(models.Model):
+    bus_name = models.CharField(max_length=50)
+    bus_number = models.CharField(max_length=20,blank=True,null=True)
+    capacity = models.PositiveIntegerField()
+    type_choices = [('AC', 'AC'), ('Non-AC', 'Non-AC')]
+    bus_type = models.CharField(max_length=10, choices=type_choices)
+    STATUS_CHOICES = [
+        ('Available', 'Available'),
+        ('No seats', 'No seats'),
+        ('Booked', 'Booked'),
+        ('Cancelled', 'Cancelled'),
+    ]
+    status = models.CharField(max_length=10, choices=STATUS_CHOICES, default='Available')
+    price = models.DecimalField(max_digits=10, decimal_places=2)
+    route = models.ForeignKey(Route, on_delete=models.CASCADE)  # Foreign key relationship
+
+    def __str__(self):
+        return f"{self.bus_name} - {self.bus_number} - {self.route.origin} to {self.route.destination}"
+
 
 class Schedule(models.Model):
     bus = models.ForeignKey(Bus, on_delete=models.CASCADE)
@@ -33,6 +47,7 @@ class Schedule(models.Model):
 
 class Booking(models.Model):
     user = models.ForeignKey(User, on_delete=models.CASCADE)
+    bus = models.ForeignKey(Bus, on_delete=models.CASCADE)
     schedule = models.ForeignKey(Schedule, on_delete=models.CASCADE)
     seat_number = models.PositiveIntegerField()
     date = models.DateField()
