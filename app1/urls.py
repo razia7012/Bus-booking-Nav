ee83ba4f32e4fc234f9d9a36d8cc985e22420855
naveenapadmakumar <naveena@codesvera.com>
2023-12-13 19:40:29 +0530

    added search functionality based on origin,destination and travel date

--- app1/urls.py
+++ app1/urls.py
@@ -4,6 +4,8 @@ from . import views
 urlpatterns=[
 
         path('',views.home,name='base'),
-        path('bus-list/',views.bus_list,name='bus-list')
+        path('bus-list/',views.bus_list,name='bus-list'),
+        path('search_buses/',views.search_buses,name='search'),
+
 
 ]
--- app1/views.py
+++ app1/views.py
@@ -13,3 +13,31 @@ def bus_list(request):
     return render(request, 'bus_list.html', {'buses': buses})
 
 
+def search_buses(request):
+    if request.method == 'POST':
+        source = request.POST.get('source', '')
+        destination = request.POST.get('destination', '')
+        travel_date = request.POST.get('travel_date', '')
+
+        if travel_date:
+            buses = Bus.objects.filter(
+                route__origin__icontains=source,
+                route__destination__icontains=destination,
+                schedule__departure_time__date=travel_date
+            )
+        else:
+            buses = Bus.objects.none()
+
+        context = {
+            'buses': buses,
+            'source': source,
+            'destination': destination,
+            'travel_date': travel_date,
+        }
+
+        if not buses:
+            context['no_buses_message'] = "No buses available for the given search criteria."
+
+        return render(request, 'search.html', context)
+    else:
+        return render(request, 'search.html')
